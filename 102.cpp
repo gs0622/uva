4a3067d51f4ba69159fb1975da764150410c84e3
Harry Pan <gs0622@gmail.com>
2014-04-19 23:09:53 +0800

    update for WA

--- 102.cpp
+++ 102.cpp
@@ -18,24 +18,28 @@ The Input:
 
 using namespace std;
 
-enum { B=0, G=1, C=2 };
+enum { B=0, C=1, G=2 };
 
 int main(void)
 {
-    char state[6][3] = { {B,G,C}, {C,B,G}, {G,C,B},
-                         {C,G,B}, {B,C,G}, {G,B,C} };
-    char str[6][4] = { "BGC", "CBG", "GCB", "CGB", "BCG", "GBC" };
-    int i, bin[3][3], all, mov, min, ret;
+/* Why WA?
+If more than one order of brown, green, and clear bins 
+yields the minimum number of movements then the alphabetically
+first string representing a minimal configuration should be printed.
+*/
+    char state[6][3] = { {B,C,G}, {B,G,C}, {C,B,G},
+                         {C,G,B}, {G,B,C}, {G,C,B} };
+    char str[6][4] = { "BCG", "BGC", "CBG", "CGB", "GBC", "GCB" };
+    int i, bin[3][3]/*BGC order*/, all, mov, min, ret;
 
-    while (cin >> bin[0][0] >> bin[0][1] >> bin[0][2]
-               >> bin[1][0] >> bin[1][1] >> bin[1][2]
-               >> bin[2][0] >> bin[2][1] >> bin[2][2]) {
+    while (cin >> bin[0][B] >> bin[0][G] >> bin[0][C]
+               >> bin[1][B] >> bin[1][G] >> bin[1][C]
+               >> bin[2][B] >> bin[2][G] >> bin[2][C]) {
         for (i=0, all=0; i<9; i++) all+=bin[(i<3)?0:(i<6)?1:2][i%3];
         for (i=0, min=all; i<6; i++) {
             mov = all - bin[0][state[i][0]]
                       - bin[1][state[i][1]]
                       - bin[2][state[i][2]];
-            //cout << mov << " ";
             if (min > mov) {
                 min = mov;
                 ret = i;
