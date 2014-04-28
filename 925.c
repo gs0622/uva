d02966ab6c56052fdf12744ad7660ea731d1f62a
Harry Pan <gs0622@gmail.com>
2014-04-28 23:31:27 +0800

    DAG version, however unknown reason still WA

--- 925.c
+++ 925.c
@@ -2,9 +2,9 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define MAX 121
-char course[MAX][20];
-int dep[MAX][MAX];
+#define MAX 150
+char course[MAX][30];
+int dag[MAX][MAX];
 int pre[MAX];
 int C, k, j;
 
@@ -13,58 +13,56 @@ static int cmp(const void *p, const void *q)
 	return strcmp((char *)p, (char *)q);
 }
 
-int course_index(char *name)
+static int course_index(char *name)
 {
     int a;
     for (a=0; a<k; a++)
         if (0==strcmp(name, &course[a][0]))
             return a;
-    while (1); /*trap*/
+    return 0;
 }
 
 int main(void)
 {
-#if 0
-    freopen("925.in", "r", stdin);
-#endif
     scanf("%d", &C);
     while (C--) { /*case*/
-        char name[20], name2[20];
+        char name[30]={}, name2[30]={};
         int i, a, b, c, prerequisites;
-        bzero((void *)dep, sizeof(dep));
-        bzero((void *)pre, sizeof(pre));
+        memset((void *)dag, 0, sizeof(dag));
+        memset((void *)pre, 0, sizeof(pre));
+        memset((void *)course, 0, sizeof(course));
         scanf("%d", &k);
         for (i=0;i<k;i++) scanf("%s", &(course[i][0]));
-        qsort(&(course[0][0]), k, 20, cmp); /*sort course lexicographically*/
+        qsort(course, k, 30, cmp); /*sort course lexicographically*/
         scanf("%d", &j);
-        for (i=0;i<j;i++) { /*scan dependancy*/
-            scanf("%s %d", &name, &prerequisites);
-            a = course_index(name);
+        for (i=0;i<j;i++) { /*scan DAG*/
+            scanf("%s %d", name, &prerequisites);
+            b = course_index(name);
             while (prerequisites--) {
                 scanf("%s", name2);
-                b = course_index(name2);
-                dep[a][b]=1;
+                a = course_index(name2);
+                dag[a][b]=1;
             }
         }
-        for (a=0;a<k;a++) /*build full dependancy*/
+        for (a=0;a<k;a++) /*build extended DAG*/
             for (b=0;b<k;b++)
                 for (c=0;c<k;c++)
-                    dep[a][b]|=dep[a][c]&dep[c][b];
-        for (a=0;a<k;a++) /*build shotest dependancy*/
+                    dag[a][b]|=dag[a][c]&dag[c][b];
+        for (a=0;a<k;a++) /*build shotest DAG*/
             for (b=0;b<k;b++)
                 for (c=0;c<k;c++)
-                    if (dep[a][b]==1)
-                        dep[a][b]^=dep[a][c]&dep[c][b];
-        for (a=0;a<k;a++) /*count dependancy*/
+                    if (dag[a][b]==1)
+                        dag[a][b]^=dag[a][c]&dag[c][b];
+        for (a=0;a<k;a++) /*count shotest prerequisites*/
             for (b=0;b<k;b++)
-                if (dep[a][b]==1)
-                    pre[a]+=1;
+                if (dag[a][b]==1)
+                    pre[b]+=1;
         for (a=0;a<k;a++) { /*output*/
             if (pre[a]>0) {
-                printf("%s %d ", &course[a][0], pre[a]);
+                printf("%s %d", &course[a][0], pre[a]);
                 for (b=0;b<k;b++)
-                    if (dep[a][b]==1)
-                        printf("%s ", &course[b][0]);
+                    if (dag[b][a]==1)
+                        printf(" %s", &course[b][0]);
                 printf("\n");
             }
         }
