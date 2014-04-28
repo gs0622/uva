8ab949d4fb45d056099671a00993006be22038fb
Harry Pan <gs0622@gmail.com>
2014-04-28 20:12:07 +0800

    dependancy solution, TODO: revise to DAG

--- 925.c
+++ 925.c
@@ -1,82 +1,72 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 #define MAX 121
-int adj[MAX][MAX];
-int visit[MAX];
-int parent[MAX];
 char course[MAX][20];
-int depth;
+int dep[MAX][MAX];
+int pre[MAX];
 int C, k, j;
+
+static int cmp(const void *p, const void *q)
+{
+	return strcmp((char *)p, (char *)q);
+}
 
 int course_index(char *name)
 {
     int a;
-    for (a=0; a<k; a++) {
-        if (0==strcmp(name, &course[a][0])) return a;
-    }
+    for (a=0; a<k; a++)
+        if (0==strcmp(name, &course[a][0]))
+            return a;
     while (1); /*trap*/
-}
-
-void dfs(int s)
-{
-    int j;
-    visit[s] = 1;
-    for (j=0; j<MAX; j++) {
-        if (adj[s][j] && !visit[j]) {
-            parent[j] = s;
-            depth++;
-            dfs(j);
-        }
-    }
-    printf("%s ", &(course[s][0]));
 }
 
 int main(void)
 {
-#if 1
+#if 0
     freopen("925.in", "r", stdin);
 #endif
     scanf("%d", &C);
     while (C--) { /*case*/
-        int i;
-        bzero((void *)adj, sizeof(adj));
-        bzero((void *)visit, sizeof(visit));
-        bzero((void *)parent, sizeof(parent));
+        char name[20], name2[20];
+        int i, a, b, c, prerequisites;
+        bzero((void *)dep, sizeof(dep));
+        bzero((void *)pre, sizeof(pre));
         scanf("%d", &k);
-        //printf("k=%d\n", k);
-        for (i=0;i<k;i++) {
-            scanf("%s", &(course[i][0]));
-            //printf("%d:%s ", i, &(course[i][0]));
-        }
+        for (i=0;i<k;i++) scanf("%s", &(course[i][0]));
+        qsort(&(course[0][0]), k, 20, cmp); /*sort course lexicographically*/
         scanf("%d", &j);
-        for (i=0;i<j;i++) {
-            char name[20];
-            int prerequisites, idx;
+        for (i=0;i<j;i++) { /*scan dependancy*/
             scanf("%s %d", &name, &prerequisites);
-            idx = course_index(name);
-            //printf("%s-%d-%d ", name, idx, prerequisites);
+            a = course_index(name);
             while (prerequisites--) {
-                int idx2;
-                scanf("%s", name);
-                idx2 = course_index(name);
-                //printf("%d:%d ", idx, idx);
-                adj[idx2][idx] = 1;
+                scanf("%s", name2);
+                b = course_index(name2);
+                dep[a][b]=1;
             }
-            /*parent[idx] = 0;
-            depth = 0;
-            dfs(idx);
-            printf ("d:%s %d \n", &course[idx][0], depth);
-            */
         }
-        for (i=0;i<k;i++) {
-            bzero((void *)visit, sizeof(visit));
-            bzero((void *)parent, sizeof(parent));
-            parent[i] = -1;
-            depth = 0;
-            dfs(i);
-            printf("\n");
-            //printf ("d: %s %d \n", &course[i][0], depth);
+        for (a=0;a<k;a++) /*build full dependancy*/
+            for (b=0;b<k;b++)
+                for (c=0;c<k;c++)
+                    dep[a][b]|=dep[a][c]&dep[c][b];
+        for (a=0;a<k;a++) /*build shotest dependancy*/
+            for (b=0;b<k;b++)
+                for (c=0;c<k;c++)
+                    if (dep[a][b]==1)
+                        dep[a][b]^=dep[a][c]&dep[c][b];
+        for (a=0;a<k;a++) /*count dependancy*/
+            for (b=0;b<k;b++)
+                if (dep[a][b]==1)
+                    pre[a]+=1;
+        for (a=0;a<k;a++) { /*output*/
+            if (pre[a]>0) {
+                printf("%s %d ", &course[a][0], pre[a]);
+                for (b=0;b<k;b++)
+                    if (dep[a][b]==1)
+                        printf("%s ", &course[b][0]);
+                printf("\n");
+            }
         }
     }
     return 0;
